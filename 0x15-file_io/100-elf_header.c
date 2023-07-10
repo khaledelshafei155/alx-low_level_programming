e4bce8838810375bb32ebd2fb418210f1ee8f156
khaledelshafei155 <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@github.com>
2023-07-10 11:01:59 -0700

    C - File I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,11 +10,11 @@
  */
 void close_elf(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-        exit(98);
-    }
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+		exit(98);
+	}
 }
 
 /**
@@ -23,14 +23,14 @@ void close_elf(int fd)
  */
 void check_elf(unsigned char *e_ident)
 {
-    if (e_ident[EI_MAG0] != ELFMAG0 ||
-        e_ident[EI_MAG1] != ELFMAG1 ||
-        e_ident[EI_MAG2] != ELFMAG2 ||
-        e_ident[EI_MAG3] != ELFMAG3)
-    {
-        dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-        exit(98);
-    }
+	if (e_ident[EI_MAG0] != ELFMAG0 ||
+	    e_ident[EI_MAG1] != ELFMAG1 ||
+	    e_ident[EI_MAG2] != ELFMAG2 ||
+	    e_ident[EI_MAG3] != ELFMAG3)
+	{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+		exit(98);
+	}
 }
 
 /**
@@ -39,12 +39,12 @@ void check_elf(unsigned char *e_ident)
  */
 void print_elf_ident(unsigned char *e_ident)
 {
-    int i;
+	int i;
 
-    printf("  Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf(" %02x", e_ident[i]);
-    printf("\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf(" %02x", e_ident[i]);
+	printf("\n");
 }
 
 /**
@@ -55,29 +55,29 @@ void print_elf_ident(unsigned char *e_ident)
  */
 void print_type_and_entry(uint16_t e_type, uint64_t e_entry, unsigned char *e_ident)
 {
-    const char *type_str;
+	const char *type_str;
 
-    if (e_ident[EI_CLASS] == ELFCLASS32)
-    {
-        type_str = e_type == ET_EXEC ? "EXEC (Executable file)" :
-                   e_type == ET_DYN ? "DYN (Shared object file)" :
-                   e_type == ET_REL ? "REL (Relocatable file)" :
-                   "UNKNOWN";
-    }
-    else if (e_ident[EI_CLASS] == ELFCLASS64)
-    {
-        type_str = e_type == ET_EXEC ? "EXEC (Executable file)" :
-                   e_type == ET_DYN ? "DYN (Shared object file)" :
-                   e_type == ET_REL ? "REL (Relocatable file)" :
-                   "UNKNOWN";
-    }
-    else
-    {
-        type_str = "UNKNOWN";
-    }
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+	{
+		type_str = e_type == ET_EXEC ? "EXEC (Executable file)" :
+			   e_type == ET_DYN ? "DYN (Shared object file)" :
+			   e_type == ET_REL ? "REL (Relocatable file)" :
+			   "UNKNOWN";
+	}
+	else if (e_ident[EI_CLASS] == ELFCLASS64)
+	{
+		type_str = e_type == ET_EXEC ? "EXEC (Executable file)" :
+			   e_type == ET_DYN ? "DYN (Shared object file)" :
+			   e_type == ET_REL ? "REL (Relocatable file)" :
+			   "UNKNOWN";
+	}
+	else
+	{
+		type_str = "UNKNOWN";
+	}
 
-    printf("  Type:    %s\n", type_str);
-    printf("  Entry point address:               0x%lx\n", e_entry);
+	printf("  Type:    %s\n", type_str);
+	printf("  Entry point address:               0x%lx\n", e_entry);
 }
 
 /**
@@ -89,45 +89,45 @@ void print_type_and_entry(uint16_t e_type, uint64_t e_entry, unsigned char *e_id
  */
 int main(int argc, char *argv[])
 {
-    Elf64_Ehdr *header;
-    int fd, r;
+	Elf64_Ehdr *header;
+	int fd, r;
 
-    if (argc != 2)
-    {
-        dprintf(STDERR_FILENO, "Usage: %s <ELF file>\n", argv[0]);
-        exit(98);
-    }
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s <ELF file>\n", argv[0]);
+		exit(98);
+	}
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-        exit(98);
-    }
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
 
-    header = malloc(sizeof(Elf64_Ehdr));
-    if (header == NULL)
-    {
-        close_elf(fd);
-        dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
-        exit(98);
-    }
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(fd);
+		dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
+		exit(98);
+	}
 
-    r = read(fd, header, sizeof(Elf64_Ehdr));
-    if (r == -1)
-    {
-        free(header);
-        close_elf(fd);
-        dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-        exit(98);
-    }
+	r = read(fd, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(fd);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
 
-    check_elf(header->e_ident);
-    printf("ELF Header:\n");
-    print_elf_ident(header->e_ident);
-    print_type_and_entry(header->e_type, header->e_entry, header->e_ident);
+	check_elf(header->e_ident);
+	printf("ELF Header:\n");
+	print_elf_ident(header->e_ident);
+	print_type_and_entry(header->e_type, header->e_entry, header->e_ident);
 
-    free(header);
-    close_elf(fd);
-    return 0;
+	free(header);
+	close_elf(fd);
+	return 0;
 }
